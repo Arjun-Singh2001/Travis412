ca7a01e94fe26c52b165d78161ed583f3e4cf870
Arjun-Singh2001 <115578279+Arjun-Singh2001@users.noreply.github.com>
2022-12-13 02:33:57 -0500

    Update SimpleCalculator.java

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,4 +4,13 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	public int subtract(int c, int d) {
+		return c - d;
+	}
+	public int multiply(int e, int f) {
+		return e * f;
+	}
+	public int divide(int g, int h) {
+		return g / h;
+	}
 }
